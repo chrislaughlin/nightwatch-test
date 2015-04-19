7a9de24d77dc1f1412dc0604bedbea45b5d0106b
Chris Laughlin <bluntm@gmail.com>
2015-04-19 19:23:34 +0100

    adding examples to be tested

---
+++ site/main.js
@@ -0,0 +1,11 @@
+$(function() {
+    $(".red").click(function() {
+        $(".box").css("background-color", "red")
+    });
+    $(".green").click(function() {
+        $(".box").css("background-color", "green")
+    });
+    $(".blue").click(function() {
+        $(".box").css("background-color", "blue")
+    });
+});
